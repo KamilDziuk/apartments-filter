a0f8cf40031803f1544bbe39668047c774203705
Kamil Dziuk <kamil.dziuk@o2.pl>
2023-04-16 19:25:23 +0200

    Add number building

--- script.js
+++ script.js
@@ -1,48 +1,10 @@
-//   // Dane mieszkań
-//     var mieszkania = [
-//       { cena: 200000, powierzchnia: 60, floors: 0, building: "A", link: "http://mieszkanie1.pl" },
-//       { cena: 250000, powierzchnia: 80, floors: 1, building: 0,  link: "http://mieszkanie2.pl" },
-//       { cena: 300000, powierzchnia: 100,floors: 2, building: 0,  link: "http://mieszkanie3.pl" },
-//       { cena: 350000, powierzchnia: 120,floors: 3, building: 0,  link: "http://mieszkanie4.pl" }
-//     ];
-
-//     // Funkcja wybierająca mieszkania zgodnie z kryteriami
-//     function wybierzMieszkania() {
-//       var cenaOd = Number(document.getElementById("cenaOd").value);
-//       var cenaDo = Number(document.getElementById("cenaDo").value);
-//       var powOd = Number(document.getElementById("powOd").value);
-//       var powDo = Number(document.getElementById("powDo").value);
-//       var floorsOd = Number(document.getElementById("floorsOd").value);
-//       var floorsDo = Number(document.getElementById("floorsDo").value);
-//       var buildingOd = Text(document.getElementById("buildingOd"));
-//       var buildingDo = Text(document.getElementById("buildingDo"));
-
-//       var wyniki = document.getElementById("wyniki");
-//       wyniki.innerHTML = "";
-
-//       mieszkania.forEach(function(mieszkanie) {
-//         if ((cenaOd === "" || mieszkanie.cena >= cenaOd) &&
-//             (cenaDo === "" || mieszkanie.cena <= cenaDo) &&
-//             (powOd === "" || mieszkanie.powierzchnia >= powOd) &&
-//             (powDo === "" || mieszkanie.powierzchnia <= powDo) &&
-//             (floorsDo === "" || mieszkanie.floors >= floorsDo) &&
-//             (floorsOd === "" || mieszkanie.floors <= floorsOd) &&
-//             (buildingOd === "" || mieszkanie.building >= buildingOd) &&
-//             (buildingDo === "" || mieszkanie.building <= buildingDo)) {
-//           var mieszkanieElement = document.createElement("div");
-//           mieszkanieElement.innerHTML = mieszkanie.cena + " zł | " + mieszkanie.powierzchnia + " m<sup>2</sup> | <a href='" + mieszkanie.link + "' target='_blank'>Zobacz wynik wyszukiwania</a> | " +  " Piętro:" + mieszkanie.floors +" | Budynek:"+ mieszkanie.building;
-//           wyniki.appendChild(mieszkanieElement);
-//         }
-//       });
-//     }
-    
 
       // Dane mieszkań
       var mieszkania = [
-        { cena: 200000, powierzchnia: 60, floors: 0, link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 1, link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 2, link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 4, link: "http://mieszkanie4.pl" }
+        { cena: 200000, powierzchnia: 60, floors: 0, building: "A", link: "http://mieszkanie1.pl" },
+        { cena: 250000, powierzchnia: 80, floors: 1, building: "B", link: "http://mieszkanie2.pl" },
+        { cena: 300000, powierzchnia: 100,floors: 2, building: "C", link: "http://mieszkanie3.pl" },
+        { cena: 350000, powierzchnia: 120,floors: 3, building: "D", link: "http://mieszkanie4.pl" }
       ];
   
       // Funkcja wybierająca mieszkania zgodnie z kryteriami
@@ -53,7 +15,7 @@
         var powDo = Number(document.getElementById("powDo").value);
         var floorsOd = Number(document.getElementById("floorsOd").value);
         var floorsDo = Number(document.getElementById("floorsDo").value);
-    
+          var building = document.getElementById("building");
      
   
         var wyniki = document.getElementById("wyniki");
@@ -64,11 +26,13 @@
               (cenaDo === "" || mieszkanie.cena <= cenaDo) &&
               (powOd === "" || mieszkanie.powierzchnia >= powOd) &&
               (powDo === "" || mieszkanie.powierzchnia <= powDo) &&
-              (floorsDo === "" || mieszkanie.floors >= floorsDo) &&
-              (floorsOd === "" || mieszkanie.floors <= floorsOd)) {
+              (floorsDo === "" || mieszkanie.floors <= floorsDo) &&
+              (floorsOd === "" || mieszkanie.floors >= floorsOd)) {
             var mieszkanieElement = document.createElement("div");
-            mieszkanieElement.innerHTML = mieszkanie.cena + " zł | " + mieszkanie.powierzchnia + " m<sup>2</sup> | <a href='" + mieszkanie.link + "' target='_blank'>Zobacz wynik wyszukiwania</a> | " +  " Piętro: " + mieszkanie.floors;
+            mieszkanieElement.innerHTML = "Cena: " + mieszkanie.cena + "zł | " + "Powierzchnia: "+ mieszkanie.powierzchnia + " m<sup>2</sup> | " + " Piętro: " + mieszkanie.floors +" | Budynek: "+ mieszkanie.building + "| <a href='" + mieszkanie.link + "' target='_blank'>Zobacz wynik wyszukiwania</a> ";
             wyniki.appendChild(mieszkanieElement);
+
           }
         });
       }
+
