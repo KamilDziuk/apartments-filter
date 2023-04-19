2a56ba7b8e4c483504cac83f91e46c75cf8bd0e5
Kamil Dziuk <kamil.dziuk@o2.pl>
2023-04-19 10:47:33 +0200

    Changing the language of elements

--- script.js
+++ script.js
@@ -1,110 +1,40 @@
 
       // Dane mieszkań
-      var mieszkania = [
-        { cena: 200000, powierzchnia: 58.80, floors: 0, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m1/" },
-        { cena: 250000, powierzchnia: 30.15, floors: 0, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m2/" },
-        { cena: 300000, powierzchnia: 49.20,floors: 0, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m3/" },
-        { cena: 350000, powierzchnia: 48.60,floors: 0, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m4/" },
+      var apartments = [
+        { price: 200000, surface: 58.80, floors: 0, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m1/" },
+        { price: 250000, surface: 30.15, floors: 0, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m2/" },
+        { price: 300000, surface: 49.20,floors: 0, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m3/" },
+        { price: 350000, surface: 48.60,floors: 0, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m4/" },
       
-        { cena: 200000, powierzchnia: 52.35, floors: 1, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m1-p1/" },
-        { cena: 250000, powierzchnia: 80, floors: 1, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m2-p1/" },
-        { cena: 300000, powierzchnia: 100,floors: 1, building: "A", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 1, building: "A", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 1, building: "A", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 2, building: "A", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 2, building: "A", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 2, building: "A", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 2, building: "A", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 2, building: "A", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 3, building: "A", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 3, building: "A", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 3, building: "A", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 3, building: "A", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 3, building: "A", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 4, building: "A", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 4, building: "A", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 4, building: "A", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 4, building: "A", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 4, building: "A", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 0, building: "B", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 0, building: "B", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 0, building: "B", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 0, building: "B", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 0, building: "B", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 1, building: "B", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 1, building: "B", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 1, building: "B", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 1, building: "B", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 1, building: "B", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 2, building: "B", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 2, building: "B", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 2, building: "B", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 2, building: "B", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 2, building: "B", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 3, building: "B", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 3, building: "B", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 3, building: "B", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 3, building: "B", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 3, building: "B", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 4, building: "B", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 4, building: "B", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 4, building: "B", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 4, building: "B", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 4, building: "B", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 0, building: "C", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 0, building: "C", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 0, building: "C", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 0, building: "C", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 0, building: "C", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 1, building: "C", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 1, building: "C", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 1, building: "C", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 1, building: "C", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 1, building: "C", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 2, building: "C", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 2, building: "C", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 2, building: "C", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 2, building: "C", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 2, building: "C", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 3, building: "C", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 3, building: "C", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 3, building: "C", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 3, building: "C", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 3, building: "C", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 4, building: "C", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 4, building: "C", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 4, building: "C", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 4, building: "C", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 4, building: "C", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 0, building: "D", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 0, building: "D", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 0, building: "D", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 0, building: "D", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 0, building: "D", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 1, building: "D", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 1, building: "D", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 1, building: "D", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 1, building: "D", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 1, building: "D", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 2, building: "D", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 2, building: "D", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 2, building: "D", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 2, building: "D", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 2, building: "D", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 3, building: "D", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 3, building: "D", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 3, building: "D", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 3, building: "D", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 3, building: "D", link: "http://mieszkanie4.pl" },
-        { cena: 200000, powierzchnia: 60, floors: 4, building: "D", link: "http://mieszkanie1.pl" },
-        { cena: 250000, powierzchnia: 80, floors: 4, building: "D", link: "http://mieszkanie2.pl" },
-        { cena: 300000, powierzchnia: 100,floors: 4, building: "D", link: "http://mieszkanie3.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 4, building: "D", link: "http://mieszkanie4.pl" },
-        { cena: 350000, powierzchnia: 120,floors: 4, building: "D", link: "http://mieszkanie4.pl" }
+        { price: 200000, surface: 52.35, floors: 1, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m1-p1/" },
+        { price: 200000, surface: 52.35, floors: 1, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m2-p1/" },
+        { price: 200000, surface: 52.35, floors: 1, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m3-p1/" },
+        { price: 200000, surface: 52.35, floors: 1, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m4-p1/" },
+        { price: 200000, surface: 52.35, floors: 1, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m5-p1/" },
+
+        { price: 200000, surface: 52.35, floors: 2, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m1-p2/" },
+        { price: 200000, surface: 52.35, floors: 2, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m2-p2/" },
+        { price: 200000, surface: 52.35, floors: 2, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m3-p2/" },
+        { price: 200000, surface: 52.35, floors: 2, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m4-p2/" },
+  
+        { price: 200000, surface: 52.35, floors: 3, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m1-p3/" },
+        { price: 200000, surface: 52.35, floors: 3, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m2-p3/" },
+        { price: 200000, surface: 52.35, floors: 3, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m3-p3/" },
+        { price: 200000, surface: 52.35, floors: 3, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m4-p3/" },
+
+
+        { price: 200000, surface: 52.35, floors: 4, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m1-p4/" },
+        { price: 200000, surface: 52.35, floors: 4, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m2-p4/" },
+        { price: 200000, surface: 52.35, floors: 4, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m3-p4/" },
+        { price: 200000, surface: 52.35, floors: 4, building: "A", link: "https://osiedle-paderewskiego.pl/budynek-a-m4-p4/" },
+     
+
+    
+     
       ];
   
       // Funkcja wybierająca mieszkania zgodnie z kryteriami
-      function wybierzMieszkania() {
+      function selectApartments() {
         var cenaOd = Number(document.getElementById("cenaOd").value);
         var cenaDo = Number(document.getElementById("cenaDo").value);
         var powOd = Number(document.getElementById("powOd").value);
@@ -114,21 +44,21 @@
         var building = document.getElementById("building");
      
   
-        var wyniki = document.getElementById("wyniki");
-        wyniki.innerHTML = "";
+        var result = document.getElementById("result");
+        result.innerHTML = "";
   
-        mieszkania.forEach(function(mieszkanie) {
-          if ((cenaOd === "" || mieszkanie.cena >= cenaOd) &&
-              (cenaDo === "" || mieszkanie.cena <= cenaDo) &&
-              (powOd === "" || mieszkanie.powierzchnia >= powOd) &&
-              (powDo === "" || mieszkanie.powierzchnia <= powDo) &&
-              (floorsDo === "" || mieszkanie.floors <= floorsDo) &&
-              (floorsOd === "" || mieszkanie.floors >= floorsOd)) {
-            var mieszkanieElement = document.createElement("div");
-            mieszkanieElement.innerHTML = "Cena: " + mieszkanie.cena + "zł | " + "Powierzchnia: "+ 
-            mieszkanie.powierzchnia + " m<sup>2</sup> | " + " Piętro: " + mieszkanie.floors +" | Budynek: "+ 
-            mieszkanie.building + "| <a href='" + mieszkanie.link + "' target='_blank'>Zobacz wynik wyszukiwania</a> ";
-            wyniki.appendChild(mieszkanieElement);
+        apartments.forEach(function(apartment) {
+          if ((cenaOd === "" || apartment.price >= cenaOd) &&
+              (cenaDo === "" || apartment.price <= cenaDo) &&
+              (powOd === "" || apartment.surface >= powOd) &&
+              (powDo === "" || apartment.surface <= powDo) &&
+              (floorsDo === "" || apartment.floors <= floorsDo) &&
+              (floorsOd === "" || apartment.floors >= floorsOd)) {
+            var apartmentElement = document.createElement("div");
+            apartmentElement.innerHTML = "Cena: " + apartment.price + "zł | " + "Powierzchnia: "+ 
+            apartment.surface + " m<sup>2</sup> | " + " Piętro: " + apartment.floors +" | Budynek: "+ 
+            apartment.building + "| <a href='" + apartment.link + "' target='_blank'>Zobacz wynik wyszukiwania</a> ";
+            result.appendChild(apartmentElement);
           }
         });
       }
